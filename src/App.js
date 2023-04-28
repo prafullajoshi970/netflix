24813f14bf825e64f8b22884acf3ae9c9f10e564
prafullajoshi970 <97543959+prafullajoshi970@users.noreply.github.com>
2023-04-28 08:36:05 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Data from "./component/Data"
+
+
+function App() {
+  return (
+    <div className="App">
+    <Data/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Data.js
@@ -0,0 +1,43 @@
+import { Component }  from "react";
+//'http://www.omdbapi.com/?i=tt3896198&apikey=9ffb785f'
+
+class Netflix extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+       new:'all'
+    };
+
+  }
+
+  componentDidMount() {
+    fetch(`http://www.omdbapi.com/?apikey=9ffb785f&s=${this.state.new}`)
+      .then(response => response.json())
+      .then(data => this.setState({ movies: data.Search }));
+  }
+
+  render() {
+    const { movies } = this.state;
+    return (
+      <>
+          <h1 style={{color:'red',fontFamily:'Cursive',textAlign:'center'}}>Netflix</h1>
+
+          
+       <div className="movie-list">
+        
+        {movies.map(movie => (
+          <div className="movie-card" key={movie.imdbID}>
+            <img src={movie.Poster} alt={movie.Title} />
+            <h3>{movie.Title}</h3>
+            <p>{movie.Year}</p>
+            <p>{movie.Language}</p>
+            <p>{movie.plot}</p>
+          </div>
+        ))}
+      </div></>
+     
+    );
+  }
+}
+export default Netflix;
